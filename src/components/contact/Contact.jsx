f50a72b333f2c3f98dff0f77c785cb7c634ecaa9
Jarek <jaroslawwawiorko@MacBook-Pro-Jarek.local>
2021-07-31 17:49:36 +0200

    start contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,47 @@
-import "./contact.scss"
+import "./contact.scss";
+import hands from "../../images/shake.svg";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import EmailIcon from "@material-ui/icons/Email";
 
 const Contact = () => {
-    return (
-        <div className="contact"  id="contact">
-            
-        </div>
-    )
-}
+  return (
+    <div className="contact" id="contact">
+      <div className="contact__content wrapper">
+        <div className="contact__box--left box ">
+          <h2 className="contact__heading">Kontakt</h2>
 
-export default Contact
+          <form className="contact__form" action="https://formsubmit.co/your@email.com" method="POST">
+            <input type="text" name="name" placeholder="Imię" required />
+            <input type="email" name="email" placeholder="Email" required />
+            <textarea name="message" placeholder="Wiadomość"></textarea>
+            <button className="contact__form--button buttons" type="submit">
+              Wyślij
+            </button>
+          </form>
+        </div>
+        <div className="contact__box--right box">
+          <ul className="contact__icons">
+            <li className="contact__icons--item">
+              <a href="https://www.facebook.com/norbert.niechocko">
+                <FacebookIcon />
+                /norbert.niechocko
+              </a>
+            </li>
+            <li className="contact__icons--item">
+              <LocationOnIcon />
+              Białystok
+            </li>
+            <li className="contact__icons--item">
+              <EmailIcon />
+              niechocko@wp.pl
+            </li>
+          </ul>
+          <img src={hands} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import CloseIcon from "@material-ui/icons/Close";
+import ArrowLeftIcon from "@material-ui/icons/ArrowLeft";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 import "./portfolio.scss";
 
 import img1 from "../../images/portfolio/resize_1.jpg";
@@ -8,51 +12,122 @@ import img5 from "../../images/portfolio/resize_5.jpg";
 import img6 from "../../images/portfolio/resize_6.JPG";
 import img7 from "../../images/portfolio/resize_7.jpg";
 import img8 from "../../images/portfolio/resize_8.jpg";
+import img9 from "../../images/portfolio/resize_9.jpg";
+import img10 from "../../images/portfolio/resize_10.jpg";
+import img11 from "../../images/portfolio/resize_11.JPG";
+import img12 from "../../images/portfolio/resize_12.jpg";
+import img13 from "../../images/portfolio/resize_13.jpg";
+import img14 from "../../images/portfolio/resize_14.JPG";
+import img15 from "../../images/portfolio/resize_15.jpg";
+import img16 from "../../images/portfolio/resize_16.JPG";
+import img17 from "../../images/portfolio/resize_17.JPG";
+import img18 from "../../images/portfolio/resize_18.jpg";
+import img19 from "../../images/portfolio/resize_19.jpg";
+import img20 from "../../images/portfolio/resize_20.JPG";
+import img21 from "../../images/portfolio/resize_21.JPG";
+import img22 from "../../images/portfolio/resize_22.JPG";
+import img23 from "../../images/portfolio/resize_23.JPG";
 
 const Portfolio = () => {
   let data = [
     {
-      id: 1,
       imgSrc: img1,
     },
     {
-      id: 2,
       imgSrc: img2,
     },
     {
-      id: 3,
       imgSrc: img3,
     },
     {
-      id: 4,
       imgSrc: img4,
     },
     {
-      id: 5,
       imgSrc: img5,
     },
     {
-      id: 6,
       imgSrc: img6,
     },
     {
-      id: 7,
       imgSrc: img7,
     },
     {
-      id: 8,
       imgSrc: img8,
     },
+    {
+      imgSrc: img9,
+    },
+    {
+      imgSrc: img10,
+    },
+    {
+      imgSrc: img11,
+    },
+    {
+      imgSrc: img12,
+    },
+    {
+      imgSrc: img13,
+    },
+    {
+      imgSrc: img14,
+    },
+    {
+      imgSrc: img15,
+    },
+    {
+      imgSrc: img16,
+    },
+    {
+      imgSrc: img17,
+    },
+    {
+      imgSrc: img18,
+    },
+    {
+      imgSrc: img19,
+    },
+    {
+      imgSrc: img20,
+    },
+    {
+      imgSrc: img21,
+    },
+    {
+      imgSrc: img22,
+    },
+    {
+      imgSrc: img23,
+    },
   ];
+  const [model, setModel] = useState(false);
+  const [tempImgSrc, setTempImgSrc] = useState("");
+  const length = model.length;
+  const showImage = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModel(true);
+  };
+
+  const nextImage = () => {
+    setTempImgSrc(tempImgSrc === length - 1 ? 0 : tempImgSrc + 1);
+  };
   return (
     <div className="portfolio" id="portfolio">
+      <div className={model ? "model open" : "model"}>
+        <CloseIcon className="model__close" onClick={() => setModel(false)} />
+        <ArrowLeftIcon className="left" />
+        <ArrowRightIcon className="right" onClick={nextImage} />
+        <img src={tempImgSrc} alt="" className="model__image" />
+      </div>
       <div className="portfolio__content wrapper">
-        <h2 className="title">Przykładowe prace</h2>
+        <h2 className="title">
+          Przykładowe <br />
+          prace
+        </h2>
         <div className="gallery">
           {data.map((item, i) => {
             return (
-              <div className="gallery__image" key={i}>
-                {/* <img src={item.imgSrc} alt="" className="image" /> */}
+              <div className="gallery__image" key={i} onClick={() => showImage(item.imgSrc)}>
                 <div className="gallery__image--item" style={{ backgroundImage: `url(${item.imgSrc})` }}></div>
               </div>
             );
